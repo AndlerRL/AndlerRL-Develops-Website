aba36ab7ee02af759f932bccd8d0595e7b634555
Meynor R Lucas <andre.rlucas@outlook.com>
2020-02-10 20:52:39 -0600

    meta update

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -59,56 +59,6 @@ const SEO = ({ pageView, modalView }) => {
       }}
       title={meta.siteTitle}
       titleTemplate={`${meta.siteTitle}`}
-      meta={[
-        {
-          name: 'description',
-          content: meta.siteDescription,
-        },
-        {
-          property: 'og:title',
-          content: meta.siteTitle
-        },
-        {
-          property: 'og:description',
-          content: meta.siteDescription,
-        },
-        {
-          property: 'og:type',
-          content: 'website', 
-        },
-        {
-          property: 'og:url',
-          content: meta.siteUrl,
-        },
-        {
-          property: 'og:image',
-          content: logo
-        },
-        {
-          property: 'og:image:type',
-          content: 'image/jpeg'
-        },
-        {
-          property: 'og:image:width',
-          content: '415'
-        },
-        {
-          property: 'og:image:height',
-          content: '415'
-        },
-        {
-          property: 'og:image:alt',
-          content: 'Andler Develops Logo'
-        },
-        {
-          name: 'keywords',
-          content: 'andlerrl develops,andlerrl dev,andlerrl, full-stack developer,fullstack developer,portfolio,web portfolio,freelance developer,andler devs,andler develops,andler,andler web'
-        },
-        {
-          name: 'google-site-verification',
-          content: '6c3Ga1g7xk1avVIXbJ_vdJjlp7UDuhiFxSmTjrJjPzw'
-        },
-      ].concat(meta)}
     >
       <script type="application/ld+json">{JSON.stringify(schemaOrg)}</script>
     </Helmet>
