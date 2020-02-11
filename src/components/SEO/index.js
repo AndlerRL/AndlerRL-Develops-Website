a7c878162c92c31c6f6a68b20a169705b7f8d8ac
Meynor R Lucas <andre.rlucas@outlook.com>
2020-02-10 21:20:45 -0600

    meta update

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -7,7 +7,7 @@ const SEO = ({ pageView, modalView }) => {
   useEffect(() => {
     ReactGA.initialize('UA-140783181-1', {
       debug: false,
-      titleCase: true
+      titleCase: false
     });
     pageView && ReactGA.pageview(pageView || '/');
     modalView && ReactGA.modalview(modalView);
@@ -59,6 +59,96 @@ const SEO = ({ pageView, modalView }) => {
       }}
       title={meta.siteTitle}
       titleTemplate={`${meta.siteTitle}`}
+      meta={[
+        {
+          name: 'description',
+          content: meta.siteDescription,
+        },
+        {
+          property: 'og:title',
+          content: meta.siteTitle
+        },
+        {
+          property: 'og:description',
+          content: meta.siteDescription,
+        },
+        {
+          property: 'og:type',
+          content: 'website', 
+        },
+        {
+          property: 'og:url',
+          content: meta.siteUrl,
+        },
+        {
+          property: 'og:image',
+          content: logo
+        },
+        {
+          property: 'og:image:type',
+          content: 'image/jpeg'
+        },
+        {
+          property: 'og:image:width',
+          content: '415'
+        },
+        {
+          property: 'og:image:height',
+          content: '415'
+        },
+        {
+          property: 'og:image:alt',
+          content: 'Andler Develops Logo'
+        },
+        {
+          property: 'twitter:title',
+          content: meta.siteTitle
+        },
+        {
+          property: 'twitter:description',
+          content: meta.siteDescription,
+        },
+        {
+          property: 'twitter:type',
+          content: 'website', 
+        },
+        {
+          property: 'twitter:url',
+          content: meta.siteUrl,
+        },
+        {
+          property: 'twitter:image',
+          content: logo
+        },
+        {
+          property: 'twitter:image:type',
+          content: 'image/jpeg'
+        },
+        {
+          property: 'twitter:image:width',
+          content: '415'
+        },
+        {
+          property: 'twitter:image:height',
+          content: '415'
+        },
+        {
+          property: 'twitter:image:alt',
+          content: 'Andler Develops Logo'
+        },
+        {
+          name: 'keywords',
+          content: 'andlerrl develops,andlerrl dev,andlerrl, full-stack developer,fullstack developer,portfolio,web portfolio,freelance developer,andler devs,andler develops,andler,andler web'
+        },
+        {
+          name: 'google-site-verification',
+          content: '6c3Ga1g7xk1avVIXbJ_vdJjlp7UDuhiFxSmTjrJjPzw'
+        },
+        {
+          name: 'theme-color',
+          content: '#222'
+        }
+      ].concat(meta)}
     >
       <script type="application/ld+json">{JSON.stringify(schemaOrg)}</script>
     </Helmet>
